8d26dd7b1f971a43585f45ae9cbe669962a45e9a
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-01-20 11:42:22 -0600

    Add Streaming services only get

---
+++ Controllers/ServicioController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pagos.Backend.DAL.IServices;
+using Pagos.Backend.DTO;
+
+namespace Pagos.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicioController : ControllerBase
+    {
+        public GenericDTO response;
+        private readonly IServicioProvider _servicioQuery;
+
+        public ServicioController(GenericDTO response, IServicioProvider servicioProvider)
+        {
+            this.response = response;
+            this._servicioQuery = servicioProvider;
+        }
+
+        [HttpGet]
+        public async Task<GenericDTO> GetServicesAsync()
+        {
+            try
+            {
+                var lstService = await _servicioQuery.GetServicesAsync();
+
+                response.Success = 1;
+                response.Data = lstService;
+            }
+
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+
+        [HttpGet("byid/{id}")]
+        public async Task<GenericDTO> GetServiceById(int id)
+        {
+            try
+            {
+                var service = await _servicioQuery.GetServiceByIdAsync(id);
+
+                response.Success = 1;
+                response.Data = service;
+            }
+
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
---
+++ DAL/Services/ServicioProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pagos.Backend.DAL.IServices;
+using Pagos.Backend.Data;
+
+namespace Pagos.Backend.DAL.Services
+{
+    public class ServicioProvider : IServicioProvider
+    {
+        public Task<Servicio> GetServiceByIdAsync(int id)
+        {
+            using (var db = new DeudaContext())
+            {
+                var service = db.Servicios.Where(s => s.IdServicio == id).FirstOrDefault();
+
+                return Task.FromResult(service);
+            }
+        }
+
+        public Task<ICollection<Servicio>> GetServicesAsync()
+        {
+            using (var db = new DeudaContext())
+            {
+                var lstService = db.Servicios.ToList();
+
+                return Task.FromResult((ICollection<Servicio>)lstService);
+            }
+        }
+    }
+}
