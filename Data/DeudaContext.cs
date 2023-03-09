648f14dfe05e5bfef342c8b90a0d21cb5f39836f
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-03-09 17:55:09 -0600

    Finish the Authentication

--- Data/DeudaContext.cs
+++ Data/DeudaContext.cs
@@ -29,10 +29,6 @@ public partial class DeudaContext : DbContext
 
     public virtual DbSet<UsuarioServicio> UsuarioServicios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=vicentc0de.com;database=deuda;uid=deuda;pwd=deuda.deuda2525;sslmode=none", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.3.37-mariadb"));
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
---
+++ Models/Common/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Pagos.Backend.Models.Common
+{
+    public class AppSettings
+    {
+        public string Secreto { get; set; }
+    }
+}
