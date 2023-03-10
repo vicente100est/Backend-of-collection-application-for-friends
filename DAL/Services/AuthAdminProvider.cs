3fe1225345a1f52ac0212dbdace63b3bd6b01b11
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-03-10 12:22:26 -0600

    Add Token Admin Auth

--- DAL/Services/AuthAdminProvider.cs
+++ DAL/Services/AuthAdminProvider.cs
@@ -1,8 +1,14 @@
-﻿using Pagos.Backend.DAL.IServices;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Pagos.Backend.DAL.IServices;
 using Pagos.Backend.Data;
 using Pagos.Backend.DTO;
+using Pagos.Backend.Models.Common;
 using Pagos.Backend.Models.Entity;
 using Pagos.Backend.Tools;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
 
 namespace Pagos.Backend.DAL.Services
 {
@@ -10,11 +16,13 @@ namespace Pagos.Backend.DAL.Services
     {
         public AdminDTO adminResponse;
         private readonly DeudaContext _db;
+        private readonly AppSettings _appSettings;
 
-        public AuthAdminProvider(AdminDTO response, DeudaContext db)
+        public AuthAdminProvider(AdminDTO response, DeudaContext db, IOptions<AppSettings> appSettings)
         {
             this.adminResponse = response;
             this._db = db;
+            this._appSettings = appSettings.Value;
         }
 
         public AdminDTO AuthAdmin(AdminAuthEntity adminModel)
@@ -34,10 +42,35 @@ namespace Pagos.Backend.DAL.Services
                 else
                 {
                     adminResponse.NombreAdministrador = authAdmin.NombreAdministrador;
+                    adminResponse.TokenAdministrador = GetAdminToken(authAdmin);
                 }
             }
 
             return adminResponse;
+        }
+
+        private string GetAdminToken(Administrador admin)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key =  Encoding.ASCII.GetBytes(_appSettings.Secreto);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, admin.IdAdministrador.ToString())
+                    }
+                    ),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                                            SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenAdmin = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(tokenAdmin);
         }
     }
 }
