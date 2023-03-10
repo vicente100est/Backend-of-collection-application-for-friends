b7f9189482d6c6778cae6d1f4d564b2933a9cdeb
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-03-10 16:37:35 -0600

    Add Authorize in all controllers but not in User Controller

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Pagos.Backend.DAL.IServices;
 using Pagos.Backend.DTO;
 using Pagos.Backend.Models.Entity;
@@ -7,6 +8,7 @@ namespace Pagos.Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class AdminController : ControllerBase
     {
         public GenericDTO response;
--- Controllers/StatusController.cs
+++ Controllers/StatusController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Pagos.Backend.DAL.IServices;
 using Pagos.Backend.DTO;
 
@@ -6,6 +7,7 @@ namespace Pagos.Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class StatusController : ControllerBase
     {
         public GenericDTO response;
